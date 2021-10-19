e5272f5254c210bbb21b2a56ad33cd225968efd5
气泡缓释胶囊 <japanztp@gmail.com>
2021-10-19 13:13:23 +0800

    Initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 ControllerInput;
+    private void Update()
+    {
+        // ControllerInput.x = Input.GetAxis("Horizontal");
+        // ControllerInput.y = 
+    }
+}
