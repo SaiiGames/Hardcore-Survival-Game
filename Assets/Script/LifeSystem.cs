11db784fe0a9da5823224164d824b790671d2e35
气泡缓释胶囊 <japanztp@gmail.com>
2021-10-22 20:11:53 +0800

    Completed help panel

--- Assets/Script/LifeSystem.cs
+++ Assets/Script/LifeSystem.cs
@@ -2,16 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class LifeSystem : MonoBehaviour
 {
-    public Image thirstUI, hungerUI;
+    public Image thirstUI, hungerUI,temperatureUI;
     
     private const float maxF=100, minF=0;
-    [SerializeField]private float thirst=98, hunger=98;
+    [SerializeField]private float thirst=98, hunger=98, temperature = 100;
 
-    [SerializeField] private float thirstIndex,hungetIndex;
+    [SerializeField] private float thirstIndex;
+    [FormerlySerializedAs("hungetIndex")] [SerializeField] private float hungerIndex;
+    [SerializeField] private float temperatureIndex;
+
+    private void Awake()
+    {
+        thirstUI = GameObject.Find("thirst").GetComponent<Image>();
+        hungerUI = GameObject.Find("hunger").GetComponent<Image>();
+        temperatureUI = GameObject.Find("temperature").GetComponent<Image>();
+
+    }
 
     private void Start()
     {
@@ -28,18 +39,25 @@ public class LifeSystem : MonoBehaviour
     {
         thirstUI.fillAmount = thirst / maxF;
         hungerUI.fillAmount = hunger / maxF;
+        temperatureUI.fillAmount = temperature / maxF;
     }
 
     private void UpdateAmount()
     {
-        thirst -= thirstIndex;
-        hunger -= hungetIndex;
+        thirst = thirst > maxF ? maxF - 0.5f : thirst - thirstIndex;
+        hunger = hunger > maxF ? maxF - 0.5f : hunger - hungerIndex;
+        temperature = temperature > maxF ? maxF - 0.5f : temperature - temperatureIndex;
     }
 
     public void ConsumeItem(float _hunger , float _thirst)
     {
         hunger += _hunger;
         thirst += _thirst;
+    }
+
+    public void UpdateHeat(float _heat)
+    {
+        temperature += _heat;
     }
     
 }
